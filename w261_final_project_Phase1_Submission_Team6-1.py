bb0c291973f40bc8edf250f0d258f1ec463d8327
rachelgaoMIDS <rachelgaoMIDS>
2024-03-13 18:55:08 +0000

    Add Phase1 Submission Template Notebook
    Added Phase 1 submission template notebook based on instructions and rubric

---
+++ w261_final_project_Phase1_Submission_Team6-1.py
@@ -0,0 +1,92 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Instructions:
+# MAGIC
+# MAGIC Objectives:
+# MAGIC - Explain data (i.e.., simple exploratory analysis of various fields, such as the semantic as well the intrinsic meaning of ranges, null values, categorical/numerical, mean/std.dev to normalize and/or scale inputs). Identify any missing or corrupt (i.e., outlier) data.
+# MAGIC - Define the outcome (i.e., the evaluation metric and the target) precisely, including mathematical formulas.
+# MAGIC - How do you ingest the CSV files and represent them efficiently? (think about different file formats)
+# MAGIC - Summarized relevant datasets based on three months of the joined On Time Performance and Weather  (OTPW data); see Azure storage bucket via the Databricks starting notebook
+# MAGIC - Split the data train/validation/test - making sure no leaks occur, for example: normalize using the training statistics. 
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Project Title
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Team Members
+# MAGIC
+# MAGIC - Name
+# MAGIC - Email
+# MAGIC - Photo
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Phase Leader Plan
+# MAGIC
+# MAGIC - In table format
+# MAGIC - https://docs.google.com/spreadsheets/d/1VP0OrvDxoKikjz4gOAMVXLI9-vB_LTrY79So2krrMDo/edit#gid=2061234516
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Credit assignment plan 
+# MAGIC
+# MAGIC - who does/did what
+# MAGIC - https://docs.google.com/spreadsheets/d/1VP0OrvDxoKikjz4gOAMVXLI9-vB_LTrY79So2krrMDo/edit#gid=2061234516
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Abstract
+# MAGIC
+# MAGIC - Around 150-200 words
+# MAGIC - In your own words, summarize at a high level the project, the data, your planned pipelines/experiments, metrics, etc.
+# MAGIC - https://www.aje.com/arc/make-great-first-impression-6-tips-writing-strong-abstract/
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Data description
+# MAGIC
+# MAGIC - Basic analysis and understanding of data
+# MAGIC - Include:
+# MAGIC   - What data you plan to use
+# MAGIC   - Some summary visual EDA
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Machine algorithms and metrics 
+# MAGIC
+# MAGIC - about 200-300 words
+# MAGIC - Description of algorithms to be used
+# MAGIC   - names
+# MAGIC   - implementations
+# MAGIC   - loss functions
+# MAGIC   - why
+# MAGIC - Description of metrics and analysis to be used
+# MAGIC   - include the equations for the metrics
+# MAGIC   - include both standard metrics and domain-specific metrics
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Machine Learning Pipelines
+# MAGIC
+# MAGIC - Description of pipeline steps (and a block/Gantt diagram) involved in completing this task and a timeline
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Conclusion and next steps
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Open issues or problems
